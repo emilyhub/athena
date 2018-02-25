ecbec0fbb227c3d78d974f17037516c3f3eb649d
annaq1 <annaq1@uci.edu>
2018-02-24 21:17:40 -0800

    gets user input, sends tuple of their words

--- TwilioAPI.py
+++ TwilioAPI.py
@@ -1,7 +1,8 @@
 #cloud.google.com/appengine/docs/standard/go/building-app
 #ngrok
-from flask import Flask
+from flask import Flask, request, redirect
 from twilio.rest import Client
+import UserInput
 
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -9,9 +10,15 @@ app = Flask(__name__)
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None) #type is string
+    print(body)
+    messageTuple = UserInput.Sentence_To_Words(body)
+    
     r = MessagingResponse()
-    r.message('Hello World')
+    r.message('{}'.format(messageTuple))
     return str(r)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- UserInput.py
+++ UserInput.py
@@ -1,10 +1,12 @@
 
 def Sentence_To_Words(query: str)-> [str]:
     """Converts a sentence to words"""
+    list_words = query.split();
+    return tuple(list_words)
     
-
-
-
+    
+    
+    
 
 if __name__ == "__main__":
     pass
